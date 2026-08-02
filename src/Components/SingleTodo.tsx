b65bf2aa42b1740fdaaecc553f4982d0dd447c7f
Ahtisham01 <219668890+Ahtisham-1@users.noreply.github.com>
2026-08-03 00:13:11 +0530

    Feat: Now we added the icons of edit, delte, done so when anyone is clicked we created a function for each one handleDone inside which we used the map on totoarray and set a conditionals that says if the todos id matches the id then turn the isdone boolean into true which was set to false by defaule and inside the return form element then we used another conditionals which said if todo.isdone is true then strike that element and if not then return the simple span element after that we created another function of onDelete which filters those elements whoose ids does not match with the selected element which means it shows us the elements other that those which is not clicked

--- src/Components/SingleTodo.tsx
+++ src/Components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import type { TodoModel } from "../Model";
 
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
+import TodoList from "./TodoList";
 
 interface Props {
   todo: TodoModel;
@@ -10,17 +11,35 @@ interface Props {
   setTodoArray: React.Dispatch<React.SetStateAction<TodoModel[]>>;
 }
 function SingleTodo({ todo, todoArray, setTodoArray }: Props) {
+  function handleDone(id: number) {
+    setTodoArray(
+      todoArray.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo,
+      ),
+    );
+  }
+
+  function handleDelete(id: number) {
+    setTodoArray(todoArray.filter((todo) => todo.id !== id));
+  }
+
+  
   return (
     <form>
+      {todo.isDone ? (
+        <s className="icon__buttons">{todo.todo}</s>
+      ) : (
+        <span className="icon__buttons">{todo.todo}</span>
+      )}
       <span>{todo.todo}</span>
       <div>
-        <span>
+        <span className="icon__buttons" onClick={() => handleEdit(todo.id)}>
           <AiFillEdit />
         </span>
-        <span>
+        <span className="icon__buttons" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
-        <span>
+        <span className="icon__buttons" onClick={() => handleDone(todo.id)}>
           <MdDone />
         </span>
       </div>
